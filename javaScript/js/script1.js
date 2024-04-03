9e81afb22e86834c47373a1c092b767915cec7eb
Andre-eugenio-almeida <almeida.ae@outlook.com>
2024-04-03 14:28:37 -0300

    initial commit

---
+++ javaScript/js/script1.js
@@ -0,0 +1,18 @@
+class Produto {
+
+    constructor() {
+
+    }
+
+    adicionar() {
+        alert('Vamos adiconar um produto!');
+        // ...
+    }
+
+    excluir() {
+        // ...
+        alert('Item deletado!')
+    }
+}
+
+var produto = new Produto();
